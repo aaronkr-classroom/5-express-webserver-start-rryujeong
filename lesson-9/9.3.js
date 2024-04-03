f0efdf871a98a87ef73249d7b104bd08cdc40748
rryujeong <126834727+rryujeong@users.noreply.github.com>
2024-04-03 06:28:52 +0000

    8,9,10끝 04-03

--- lesson-9/9.3.js
+++ lesson-9/9.3.js
@@ -46,6 +46,10 @@ app.get("/contact", (req, res) => {
   res.send("This is the CONTACT page.");
 });
 
+app.get("/about", (req, res) => {
+  res.send("This is the About page.");
+
+});
 /**
  * Listing 9.2 (p. 145)
  * 경로 매개변수로 응답하기
--- lesson-9/controllers/homeController.js
+++ lesson-9/controllers/homeController.js
@@ -6,14 +6,40 @@
  * 콜백 함수를 homeController.js의 홈 컨트롤러로 이동
  */
 // @TODO: exports 객체를 사용해 sendReqParam 함수를 모듈로 내보낸다
-exports.sendReqParam; // 지정 라우트 요청 처리를 위한 함수 생성
+exports.sendVegParam = (req,res) => {
+    let veg = req.params.vegetable.toUpperCase();
+    res.send(`This is the <span style ='color:purple'> ${veg}</span> page.`);
+}; // 지정 라우트 요청 처리를 위한 함수 생성
+
+exports.sendUserParam = (req,res) => {
+    let uname = req.paramas.username.toUpperCase();
+    res.send(`This is the <span style ='color:blue'> ${uname}</span> page.`);
+};
+
+
+exports.sendCourseParam = (req,res) => {
+    let course = req.paramas.courseId.toUpperCase();
+    res.send(`This is the <span style ='color:goldenrod'> ${course}</span> page.`);
+};
 
 /**
  * 다른 라우트를 옮기기
  */
-exports.getHome; // @TODO: exports 객체를 사용해 getHome 함수를 모듈로 내보낸다
-exports.getContact; // @TODO: exports 객체를 사용해 getContact 함수를 모듈로 내보낸다
-exports.postContact; // @TODO: exports 객체를 사용해 postContact 함수를 모듈로 내보낸다
+exports.getHome = (req,res) => {
+    res.send("This is the <span style ='color:red'>HOME</span> page.");
+}; // @TODO: exports 객체를 사용해 getHome 함수를 모듈로 내보낸다
+exports.getAbout = (req,res) => {
+    res.send("This is the <span style ='color:greed'>About</span> page.");
+};
+exports.getContact = (req,res) => {
+    res.send("This is the <span style ='color:orange'>Contact</span> page.");
+}; // @TODO: exports 객체를 사용해 getContact 함수를 모듈로 내보낸다
+exports.postContact = (req,res) => {
+    res.send("Contact info submitted!!!!!111!!");
+}; // @TODO: exports 객체를 사용해 postContact 함수를 모듈로 내보낸다
 
 // @TODO: exports 객체를 사용해 logRequestPaths 미들웨어 함수를 모듈로 내보낸다
-exports.logRequestPaths;
+exports.logRequestPaths = (req,res, next) => {
+    console.log(`request to: ${req.url}`);
+    next();
+};
